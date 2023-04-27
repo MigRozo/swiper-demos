853bd6ddd9e2d2b95ec10ee7ebd84a467c6af858
Mig <angelorozo13@hotmail.com>
2023-04-26 22:08:47 -0500

    JS details updated

--- main.js
+++ main.js
@@ -11,31 +11,40 @@ document.querySelector('#app').innerHTML = `
   <details>
     <summary>See JS Settings</summary>
     <pre>
-      const prevEl = sym_slides.querySelector('.swiper-arrow-prev')
-        ? sym_slides.querySelector('.swiper-arrow-prev')
-        : sym_slides.querySelector('.swiper-button-prev');
-    
-      const nextEl = sym_slides.querySelector('.swiper-arrow-next')
-        ? sym_slides.querySelector('.swiper-arrow-next')
-        : sym_slides.querySelector('.swiper-button-next');
-    
-      const swiper = new Swiper(sym_slides, {
-        modules: [ EffectFade, Navigation, Pagination ],
-        effect: sym_slides.dataset.fade !== undefined ? 'fade' : 'slide',
-        loop: true,
-        spaceBetween: 30,
-        fadeEffect: {
-          crossFade: true
-        },
-        navigation: {
-          prevEl,
-          nextEl,
-        },
-        pagination: sym_slides.dataset.dots !== undefined && {
-          el: sym_slides.querySelector('.swiper-pagination'),
-          type: 'bullets',
-          clickable: true,
-        },
+      import Swiper, { EffectFade, Navigation, Pagination } from 'swiper';
+      import 'swiper/css';
+      import 'swiper/css/effect-fade';
+      import 'swiper/css/navigation';
+      import 'swiper/css/pagination';
+      import 'swiper/css/scrollbar';
+
+      document.querySelectorAll('.sym-slides').forEach(sym_slides => {
+        const prevEl = sym_slides.querySelector('.swiper-arrow-prev')
+          ? sym_slides.querySelector('.swiper-arrow-prev')
+          : sym_slides.querySelector('.swiper-button-prev');
+      
+        const nextEl = sym_slides.querySelector('.swiper-arrow-next')
+          ? sym_slides.querySelector('.swiper-arrow-next')
+          : sym_slides.querySelector('.swiper-button-next');
+      
+        const swiper = new Swiper(sym_slides, {
+          modules: [ EffectFade, Navigation, Pagination ],
+          effect: sym_slides.dataset.fade !== undefined ? 'fade' : 'slide',
+          loop: true,
+          spaceBetween: 30,
+          fadeEffect: {
+            crossFade: true
+          },
+          navigation: {
+            prevEl,
+            nextEl,
+          },
+          pagination: sym_slides.dataset.dots !== undefined && {
+            el: sym_slides.querySelector('.swiper-pagination'),
+            type: 'bullets',
+            clickable: true,
+          },
+        });
       });
     </pre>
   </details>
